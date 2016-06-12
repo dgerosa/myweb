e1fd0243ecafc62ec153ba020ab3f37df6bf7443
Davide Gerosa <d.gerosa@damtp.cam.ac.uk>
2016-06-12 23:19:35 +0100

    script

--- update_website.sh
+++ update_website.sh
@@ -1,8 +1,14 @@
+HERE=$(pwd)
+
 # Upload this website...
 MYWEB="davidegerosa.com"
 #MYWEB="https://davidegerosa.com"
 #MYWEB="davidegerosa.wordpress.com"
 echo $MYWEB
+
+# Here is my repository
+REPDIR="${HOME}/reps/myweb"
+cd $REPDIR
 
 # Download all the website
 wget --convert-links -r "https://${MYWEB}"
@@ -14,3 +20,6 @@ scp -r $MYWEB/* dg438@ssh.damtp.cam.ac.uk:public_html
 git add $MYWEB
 git commit -m "Automatic commit from update_website.sh"
 git push
+
+# Go back
+cd $HERE
