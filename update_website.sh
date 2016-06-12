e495e83fe5cda88170e53349ab4d543945612373
Davide Gerosa <d.gerosa@damtp.cam.ac.uk>
2016-06-12 23:10:24 +0100

    script

--- update_website.sh
+++ update_website.sh
@@ -9,12 +9,8 @@ wget --convert-links -r "https://${MYWEB}"
 
 # Upload website the department server
 scp -r $MYWEB/* dg438@ssh.damtp.cam.ac.uk:public_html
-# 
-# # Store website on github
-# git add *
-# git commit -m "Automatic commit from update_website.sh"
-# git push
-# 
-# # ... and the university ones
-# #sshpass -p "Bornt0run" scp -r * dg438@linux.ds.cam.ac.uk:public_html
-# 
+
+# Store website on github
+git add $MYWEB
+git commit -m "Automatic commit from update_website.sh"
+git push
