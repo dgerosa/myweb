02cf8d1de3835f9d710790994b0ea198f822762e
Davide Gerosa <d.gerosa@damtp.cam.ac.uk>
2016-06-23 17:31:40 +0100

    default comments

--- update_website.sh
+++ update_website.sh
@@ -1,3 +1,9 @@
+# Usage:
+# bash update_website.sh "COMMENT"
+
+# Set a default if no comment is given
+COMMENT=${1:-"Automatic commit from update_website.sh"}
+
 HERE=$(pwd)
 
 # Upload this website...
@@ -18,7 +24,7 @@ scp -r $MYWEB/* dg438@ssh.damtp.cam.ac.uk:public_html
 
 # Store website on github
 git add $MYWEB
-git commit -m "Automatic commit from update_website.sh"
+git commit -m "$COMMENT"
 git push
 
 # Go back
